82e1387f09f5fdb1fb17f559a9eae62d5c250749
lorenzotelesca <139251814+lorenzotelesca@users.noreply.github.com>
2023-07-13 11:16:10 +0200

    Update SleepingBarber.c
    Aggiornamento del main che inizialmente era solo generico (aggiunta del contatore e del vettore degli argomenti e creazione array di thread e di interi)

--- SleepingBarber.c
+++ SleepingBarber.c
@@ -2,12 +2,15 @@
 #include <stdio.h>
 #include <time.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #define CLIENTI_MASSIMI 50
 
 
-int main() {
+int main(int argc, char * argv[]) {
+    pthread_t btid;
+    pthread_t tid[CLIENTI_MASSIMI];
+    int i, numClienti, numeroSedie;
+    int Numero[CLIENTI_MASSIMI];
     
-    
-    return 0;
 }
