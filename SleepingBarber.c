b8ff1643648ee9d636256cc19daf6dd716ffb721
GiuseDiCanio <139243119+GiuseDiCanio@users.noreply.github.com>
2023-07-13 11:20:57 +0200

    Update SleepingBarber.c
    Convalida per vedere che il numero inserito dall'utente sia minore dei threads cliente supportati e inizializza l'array di numeri.

--- SleepingBarber.c
+++ SleepingBarber.c
@@ -13,4 +13,19 @@ int main(int argc, char * argv[]) {
     int i, numClienti, numeroSedie;
     int Numero[CLIENTI_MASSIMI];
     
+
+    printf("INSERISCI IL NUMERO DI CLIENTI(Max 50) : ");
+    scanf("%d", & numClienti);
+    printf("INSERISCI IL NUMERO DELLE SEDIE: ");
+    scanf("%d", & numeroSedie);
+
+    if (numClienti > CLIENTI_MASSIMI) {
+    printf("IL NUMERO MASSIMO DI CLIENTI E' %d.\n", CLIENTI_MASSIMI);
+    exit(-1);
+  }
+
+  for (i = 0; i < CLIENTI_MASSIMI; i++) {
+    Numero[i] = i;
+  }
+  
 }
